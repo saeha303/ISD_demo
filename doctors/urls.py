ece8844f8607a84210d047b5bc6b9a7c69c76a17
Sumaiya Saeha <119116198+saeha303@users.noreply.github.com>
2023-09-07 03:34:15 +0600

    '' added in doctor urls.py

--- doctors/urls.py
+++ doctors/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.DashboardView.as_view(), name='doctor_dashboard'),
     path('<int:doctor_id>/', views.DashboardView.as_view(), name='doctor_dashboard'),
     path('add_prescription/', views.addSuggesions.as_view(), name='add_prescription'),
     path('create_appointment/', views.createAppointment.as_view(), name='create_appointment'),
